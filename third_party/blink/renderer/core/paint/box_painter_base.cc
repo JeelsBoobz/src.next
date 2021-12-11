bb8bed782e3dff38297260153650c89d359ac8c8
Arnaud GRANAL <arnaud@geometry.ee>
2021-12-11 06:08:29 +0000

    Update night mode handling of textboxes and divs

--- third_party/blink/renderer/core/paint/box_painter_base.cc
+++ third_party/blink/renderer/core/paint/box_painter_base.cc
@@ -255,7 +255,7 @@ void BoxPainterBase::PaintNormalBoxShadow(const PaintInfo& info,
                                           const ComputedStyle& style,
                                           PhysicalBoxSides sides_to_include,
                                           bool background_is_skipped) {
-  if (!style.BoxShadow() || info.context.IsDarkModeEnabled())
+  if (!style.BoxShadow() || style.DarkColorScheme())
     return;
   GraphicsContext& context = info.context;
 
@@ -356,7 +356,7 @@ void BoxPainterBase::PaintInsetBoxShadowWithBorderRect(
     const PhysicalRect& border_rect,
     const ComputedStyle& style,
     PhysicalBoxSides sides_to_include) {
-  if (!style.BoxShadow() || info.context.IsDarkModeEnabled())
+  if (!style.BoxShadow() || style.DarkColorScheme())
     return;
 
   auto bounds = ContouredBorderGeometry::PixelSnappedContouredInnerBorder(
@@ -368,7 +368,7 @@ void BoxPainterBase::PaintInsetBoxShadowWithInnerRect(
     const PaintInfo& info,
     const PhysicalRect& inner_rect,
     const ComputedStyle& style) {
-  if (!style.BoxShadow() || info.context.IsDarkModeEnabled())
+  if (!style.BoxShadow() || style.DarkColorScheme())
     return;
   auto bounds = ContouredBorderGeometry::PixelSnappedContouredBorderWithOutsets(
       style, inner_rect, PhysicalBoxStrut());
@@ -396,7 +396,7 @@ void BoxPainterBase::PaintInsetBoxShadow(const PaintInfo& info,
                                          const ContouredRect& bounds,
                                          const ComputedStyle& style,
                                          PhysicalBoxSides sides_to_include) {
-  if (info.context.IsDarkModeEnabled())
+  if (style.DarkColorScheme())
     return;
   GraphicsContext& context = info.context;
 
